e8df93611953423606ec6513d78616ed13087e62
Towhid Chowdhury <mac9908@rit.edu>
2023-06-30 17:10:27 -0600

    Updates

---
+++ precache-manifest.eb5ec8cc074b547b313de13dfac7b7cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f88a77cd0b4bd40745d02acca377a12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8890f6c2887c8d00765",
+    "url": "./static/css/2.e53a2e28.chunk.css"
+  },
+  {
+    "revision": "62e8e8f868170dc1869c",
+    "url": "./static/css/main.63e8d9e1.chunk.css"
+  },
+  {
+    "revision": "a8890f6c2887c8d00765",
+    "url": "./static/js/2.ab5ed48c.chunk.js"
+  },
+  {
+    "revision": "67f707278e779c717094b193dc705b26",
+    "url": "./static/js/2.ab5ed48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e8e8f868170dc1869c",
+    "url": "./static/js/main.af1c776e.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
